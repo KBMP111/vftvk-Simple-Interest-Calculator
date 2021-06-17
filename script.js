35b237cb88f1e1492ef34771148f961cec4bd3ba
KBMP111 <kbmpundu@outllok.com>
2021-06-17 18:18:53 +0000

    further changes

--- script.js
+++ script.js
@@ -3,4 +3,7 @@ function compute()
     p = document.getElementById("principal").value;
     
 }
-        
+
+function updateRateValue(val) {
+          document.getElementById('rateValue').value=val; 
+        }
