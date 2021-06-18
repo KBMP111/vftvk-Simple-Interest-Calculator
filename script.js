19dfc196b87ceec59706d543427d5f6f11bbf56c
KBMP111 <kbmpundu@outlook.com>
2021-06-18 10:02:19 +0000

    text commenting

--- script.js
+++ script.js
@@ -2,9 +2,12 @@ function compute(){
     p = document.getElementById("principal").value;
     r =  document.getElementById("rate").value;
     y = document.getElementById("years").value;
-    var interest =r*y*p/100
-    document.getElementById("output").innerHTML = interest
-  
+    yrs = new Date().getFullYear()+y
+    var interest =r*y*p/100;
+    document.getElementById("output1").innerHTML = "If you deposit "+ p + ",";
+    document.getElementById("output2").innerHTML = "at an interest rate of "+r+".";
+    document.getElementById("output3").innerHTML = "You will recieve an amount of " +interest+","
+    document.getElementById("output4").innerHTML = "in the year "+ yrs
 }
 
 function updateRateValue(val) {
