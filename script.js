22b76a926267d9e4eb5de91b58ccc2bad794597e
KBMP111 <kbmpundu@outlook.com>
2021-06-18 07:50:23 +0000

    title and box

--- script.js
+++ script.js
@@ -11,5 +11,8 @@ function updateRateValue(val) {
 function updateRate(val) {
           document.getElementById('rate').value=val; 
         }
-var str = "Yeah"
-document.getElementById("output").innerHTML = str
+var str = "Yeah";
+document.getElementById("output").innerHTML = str;
+var amount = document.getElementById("principal");
+var rate =  document.getElementById("rate");
+var years = document.getElementById("years")
