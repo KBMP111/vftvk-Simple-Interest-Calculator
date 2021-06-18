2f9fc028834b36dc52e4e19e40c36ce36f8c9192
KBMP111 <kbmpundu@outlook.com>
2021-06-18 16:56:38 +0000

    hgj

--- script.js
+++ script.js
@@ -4,6 +4,13 @@ function compute(){
     y = document.getElementById("years").value;
     yrs = new Date().getFullYear()+Number(y)
     var interest =r*y*p/100;
+
+    if(p < 1) {
+        alert("Enter a positive number");
+        document.getElementById("principal").focus();
+    }
+    else
+
     document.getElementById("output1").innerHTML = "If you deposit "+ "<mark>"+p+"</mark>"  + ",";
     document.getElementById("output2").innerHTML = "at an interest rate of "+"<mark>"+r+"</mark>"+"%.";
     document.getElementById("output3").innerHTML = "You will receive an amount of " +"<mark>"+interest+"</mark>"+","
