0f829db2507e8753b23e598212d60495d3ccd4f4
KBMP111 <kbmpundu@outlook.com>
2021-06-18 10:38:21 +0000

    highlighting

--- script.js
+++ script.js
@@ -4,10 +4,10 @@ function compute(){
     y = document.getElementById("years").value;
     yrs = new Date().getFullYear()+y
     var interest =r*y*p/100;
-    document.getElementById("output1").innerHTML = "If you deposit "+ p + ",";
-    document.getElementById("output2").innerHTML = "at an interest rate of "+r+".";
-    document.getElementById("output3").innerHTML = "You will recieve an amount of " +interest+","
-    document.getElementById("output4").innerHTML = "in the year "+ yrs
+    document.getElementById("output1").innerHTML = "If you deposit "+ "<mark>"+p+"</mark>"  + ",";
+    document.getElementById("output2").innerHTML = "at an interest rate of "+"<mark>"+r+"</mark>"+".";
+    document.getElementById("output3").innerHTML = "You will recieve an amount of " +"<mark>"+interest+"</mark>"+","
+    document.getElementById("output4").innerHTML = "in the year "+ "<mark>"+yrs+"</mark>"
 }
 
 function updateRateValue(val) {
